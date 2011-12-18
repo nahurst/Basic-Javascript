d84a702c5f516fc1c6cd3895cbff8599f5fef5f7
nhurst <nahurst@gmail.com>
2011-12-18 16:55:47 -0500

    init

---
+++ application.js
@@ -0,0 +1,43 @@
+var APP = APP || {};
+
+APP = {
+  common: {
+    init: function() {
+
+    }
+  },
+  users: {
+    init: function() {
+
+    },
+    "new": function() {
+      APP.home.create();
+    },
+    create: function() {
+
+    }
+  }
+}
+UTIL = {
+  exec : function(controller, action){
+    var ns = APP,
+    action = (action === undefined) ? "init" : action;
+
+    if (controller !== "" &&
+      ns[controller] &&
+      typeof ns[controller][action] === "function" ) {
+      ns[controller][action]();
+    }
+  },
+  init: function() {
+    var body = document.body,
+    controller = body.getAttribute("data-controller"),
+    action = body.getAttribute("data-action");
+    UTIL.exec("common");
+    UTIL.exec(controller);
+    UTIL.exec(controller, action);
+  }
+};
+
+$(document).ready(UTIL.init);
+
